5a79c98fa129d002ac388a33e9be1139dd420cfb
Dams <macbookair@MacBooks-MacBook-Air.local>
2026-01-19 04:11:52 -0800

    initial commit

---
+++ projects/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from .views import ProjectListCreateView, ProjectDetailView, TaskListCreateView, \
+TaskDetailView, RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
+
+urlpatterns = [
+    path('projects/', ProjectListCreateView.as_view(), name='project-list-create'),
+    path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
+    path('projects/<int:project_id>/tasks/', TaskListCreateView.as_view(), name='task-list-create'),
+    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
+
+    # Auth
+    path('auth/register/', RegisterView.as_view(), name='auth-register'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='auth-login'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+]
+
